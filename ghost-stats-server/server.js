58667c1d49b504a0aca5727b106dee09cf67c843
OrlandoQuintana <opinoquintana@yahoo.com>
2023-04-13 22:49:04 -0500

    Added ghost_metadata table and endpoints

---
+++ ghost-stats-server/server.js
@@ -0,0 +1,198 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 3000;
+
+// Connect to SQLite database
+const db = new sqlite3.Database(':memory:', (err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+  } else {
+    console.log('Connected to SQLite database');
+  }
+});
+
+// Load JSON data into database
+const ghostStats = require('./ghoststats.json');
+const createTableQuery = `
+CREATE TABLE IF NOT EXISTS ghosts (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  rarity_rank INTEGER,
+  rank_modifier REAL,
+  stamina INTEGER,
+  smarts INTEGER,
+  luck INTEGER,
+  speed INTEGER,
+  meadows_modifier REAL,
+  dunes_modifier REAL,
+  tundra_modifier REAL,
+  deadlands_modifier REAL,
+  toxic_swamps_modifier REAL,
+  scorched_earth_modifier REAL,
+  twilight_forest_modifier REAL,
+  cyber_district_modifier REAL,
+  alien_modifier REAL,
+  farming_length TEXT
+);`;
+
+db.serialize(() => {
+  db.run(createTableQuery, (err) => {
+    if (err) {
+      console.error(err.message);
+    } else {
+      console.log('Ghost table created');
+      ghostStats.forEach((ghost) => {
+        const insertGhostQuery = `
+        INSERT INTO ghosts (
+          id, name, rarity_rank, rank_modifier, stamina, smarts, luck, speed,
+          meadows_modifier, dunes_modifier, tundra_modifier, deadlands_modifier,
+          toxic_swamps_modifier, scorched_earth_modifier, twilight_forest_modifier,
+          cyber_district_modifier, alien_modifier, farming_length
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+        `;
+        const params = [
+          ghost['Ghost ID'], ghost['Ghost Name'], ghost['Rarity Rank'],
+          ghost['Rank Modifier'], ghost['Stamina'], ghost['Smarts'],
+          ghost['Luck'], ghost['Speed'], ghost['Meadows Modifier'],
+          ghost['Dunes Modifier'], ghost['Tundra Modifier'],
+          ghost['Deadlands Modifier'], ghost['Toxic Swamps Modifier'],
+          ghost['Scorched Earth Modifier'], ghost['Twilight Forest Modifier'],
+          ghost['Cyber District Modifier'], ghost['Alien Modifier'],
+          ghost['Farming Length'],
+        ];
+        db.run(insertGhostQuery, params, (err) => {
+          if (err) {
+            console.error(err.message);
+          }
+        });
+      });
+    }
+  });
+});
+
+// API endpoint to get all ghosts
+app.get('/api/ghosts', (req, res) => {
+  const query = 'SELECT * FROM ghosts;';
+  db.all(query, [], (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).json({ error: 'An error occurred while fetching ghosts.' });
+    } else {
+      res.json(rows);
+    }
+  });
+});
+
+// API endpoint to get a specific ghost by ID
+app.get('/api/ghosts/:id', (req, res) => {
+  const id = req.params.id;
+  const query = 'SELECT * FROM ghosts WHERE id = ?;';
+  db.get(query, [id], (err, row) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).json({ error: 'An error occurred while fetching the ghost.' });
+    } else if (row) {
+      res.json(row);
+    } else {
+      res.status(404).json({ error: 'Ghost not found.' });
+    }
+  });
+});
+
+
+// Load JSON data for ghost_metadata into the database
+const ghostMetadata = require('./ghostmetadata.json');
+const createMetadataTableQuery = `
+CREATE TABLE IF NOT EXISTS ghost_metadata (
+  id INTEGER PRIMARY KEY,
+  backdrop TEXT,
+  background TEXT,
+  backpack TEXT,
+  blastoff TEXT,
+  body TEXT,
+  eyes TEXT,
+  face TEXT,
+  glasses TEXT,
+  hands TEXT,
+  hat TEXT,
+  hideme TEXT,
+  html TEXT,
+  name TEXT,
+  outfit TEXT,
+  rank INTEGER,
+  score REAL,
+  special TEXT,
+  svg TEXT,
+  varatts INTEGER
+);`;
+
+db.serialize(() => {
+  db.run(createMetadataTableQuery, (err) => {
+    if (err) {
+      console.error(err.message);
+    } else {
+      console.log('Ghost Metadata table created');
+      ghostMetadata.forEach((metadata) => {
+        const insertMetadataQuery = `
+        INSERT INTO ghost_metadata (
+          id, backdrop, background, backpack, blastoff, body, eyes, face, glasses,
+          hands, hat, hideme, html, name, outfit, rank, score, special, svg, varatts
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+        `;
+        const params = [
+          metadata['id'], metadata['backdrop'], metadata['background'],
+          metadata['backpack'], metadata['blastoff'], metadata['body'],
+          metadata['eyes'], metadata['face'], metadata['glasses'],
+          metadata['hands'], metadata['hat'], metadata['hideme'],
+          metadata['html'], metadata['name'], metadata['outfit'],
+          metadata['rank'], metadata['score'], metadata['special'],
+          metadata['svg'], metadata['varatts'],
+        ];
+        db.run(insertMetadataQuery, params, (err) => {
+          if (err) {
+            console.error(err.message);
+          }
+        });
+      });
+    }
+  });
+});
+
+// API endpoint to get all ghost metadata
+app.get('/api/ghostmetadata', (req, res) => {
+  const query = 'SELECT * FROM ghost_metadata;';
+  db.all(query, [], (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).json({ error: 'An error occurred while fetching ghost metadata.' });
+    } else {
+      res.json(rows);
+    }
+  });
+});
+
+// API endpoint to get a specific ghost metadata by ID
+app.get('/api/ghostmetadata/:id', (req, res) => {
+  const id = req.params.id;
+  const query = 'SELECT * FROM ghost_metadata WHERE id = ?;';
+  db.get(query, [id], (err, row) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).json({ error: 'An error occurred while fetching the ghost metadata.' });
+    } else if (row) {
+      res.json(row);
+    } else {
+      res.status(404).json({ error: 'Ghost metadata not found.' });
+    }
+  });
+});
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
