f5db9c060fc5124d90f990fea845cd5cf976660d
OrlandoQuintana <opinoquintana@yahoo.com>
2023-04-25 00:10:35 -0500

    react app 1.6

--- clumsyreactapp/src/MiddleSection.js
+++ clumsyreactapp/src/MiddleSection.js
@@ -4,7 +4,7 @@ function MiddleSection() {
     return (
         <div className="section">
             <h1>Middle Section</h1>
-            <img src={process.env.PUBLIC_URL + '/picture.png'} alt="A picture" />
+            <img src={process.env.PUBLIC_URL + '/clumsyreactapp/public/picture.png'} alt="A picture" />
         </div>
     );
 }
