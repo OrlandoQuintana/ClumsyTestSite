72d0203d9708516f66e73fd050eb235b40a2e9b7
OrlandoQuintana <opinoquintana@yahoo.com>
2023-05-10 11:25:06 -0500

    clumsyview 3.1

---
+++ clumsyview/precache-manifest.12ecf1d8eae73eaed24c45fcdd90a0a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "42f4b49bb39b657b35ae",
+    "url": "/clumsyview/static/css/main.756b98a5.chunk.css"
+  },
+  {
+    "revision": "42f4b49bb39b657b35ae",
+    "url": "/clumsyview/static/js/main.5a89385b.chunk.js"
+  },
+  {
+    "revision": "7eda9686cbb3fd93f777",
+    "url": "/clumsyview/static/js/runtime~main.61591813.js"
+  },
+  {
+    "revision": "7e7f16cb6253c41a86bb",
+    "url": "/clumsyview/static/css/2.c3e049f4.chunk.css"
+  },
+  {
+    "revision": "7e7f16cb6253c41a86bb",
+    "url": "/clumsyview/static/js/2.94135b97.chunk.js"
+  },
+  {
+    "revision": "9e2d897a76c17d18ff55",
+    "url": "/clumsyview/static/js/3.3bc8c1cd.chunk.js"
+  },
+  {
+    "revision": "822981cd6625839e6cb63d4af8482e59",
+    "url": "/clumsyview/index.html"
+  }
+];
